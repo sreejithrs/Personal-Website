7450815d52453e3ebaeaea83bbc6fee88a4d43e9
sreejithrs <sreejithrs001@gmail.com>
2021-12-28 15:43:00 +0530

    Added All

---
+++ js/script.js
@@ -0,0 +1,34 @@
+$(document).ready(function(){
+    $("#submit-form").validate({
+        rules:{
+            name:{
+                required:true
+            },
+            email:{
+                required:true,
+                email:true
+            },
+            sub:{
+                required:true,
+                minlength:5
+            },
+            comments:{
+                required:true,
+                minlength:10,
+                maxlength:400
+            },
+            mob:{
+                required:true,
+                minlength:10,
+                maxlength:10
+            }
+        },
+        messages:{
+            mob:{
+                required:"Enter you mobile number",
+                minlength:"Mobile number should contain 10 numbers",
+                maxlength:"Should not exceed 10 numbers"
+            }
+        }
+    })
+})
